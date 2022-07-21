abcf270149a501be10cf2b65f7f08aadf43f6202
gil-arreola <58606087+gil-arreola@users.noreply.github.com>
2022-07-21 10:44:07 -0500

    Initial commit

---
+++ soles.js
@@ -0,0 +1,5 @@
+const conocete = document.getElementById("conocete")
+conocete.addEventListener("click",()=> alert("Recibi un click"))
+
+const aceites =  document.getElementById('aceites')
+aceites.classList.add('animated', 'bounceOutLeft')
